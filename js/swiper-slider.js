c92955f200086185f53b99aa56ba52ae82cb7867
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-24 05:46:04 -0500

    Second commit

---
+++ js/swiper-slider.js
@@ -0,0 +1,15 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+// Jobs slider
+const swiper = new Swiper('.swiper-container', {
+  
+  // Parameters
+  // centeredSlides: true,
+  direction: 'horizontal',
+  grabCursor: true,
+  slidesPerView: 'auto'
+
+});
