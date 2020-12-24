fe5cff927490ca4d412ad95d01d8e46839ce0a6a
Aldo Trujillo <aldo.trujillo96@gmail.com>
2020-12-24 09:49:28 -0500

    Commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-// const swiperSlider = require("swiper")
+// START: Swiper Slider
+
+var Swiper = require('swiper/swiper-bundle');
 
 // Jobs slider
-const swiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('.swiper-container', {
   
   // Parameters
   // centeredSlides: true,
   direction: 'horizontal',
   grabCursor: true,
   slidesPerView: 'auto'
-
+  
 });
+
+// END: Swiper Slider
